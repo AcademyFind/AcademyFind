2a883388c299855585e25b11c38fdd0240f16c07
Monish <monishmathurt@gmail.com>
2026-07-30 19:28:52 +0530

    added any implict type

--- app/sitemap-blogs.xml/route.ts
+++ app/sitemap-blogs.xml/route.ts
@@ -14,16 +14,16 @@ export async function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${blogs
-    .map(
-      (blog) => `
+      .map(
+        (blog: any) => `
   <url>
     <loc>${baseUrl}/blog/${blog.slug}</loc>
     <lastmod>${blog.updatedAt.toISOString()}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>`
-    )
-    .join('')}
+      )
+      .join('')}
 </urlset>`;
 
   return new Response(xml, {
